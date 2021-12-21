48b44fa7ae08e75cde52703646049261f6599d9a
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2021-12-21 10:53:01 -0300

    Utils removed cuz don't needed

--- dags/insert_atendime.py
+++ dags/insert_atendime.py
@@ -4,7 +4,6 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from connections.oracle.connections import connect_rhp
 from collections import OrderedDict as od
-from utils.integrity_checker import notify_email
 import unidecode
 
 from queries.rhp.atendime import query_atendime
@@ -20,7 +19,7 @@ default_args = {
     "depends_on_past": False,
     "start_date": START_DATE,
     "email": ["raphael.queiroz@eximio.med.br"],
-    "email_on_failure": True,
+    "email_on_failure": False,
     "email_on_retry": False,
     "retries": 0,
     "retry_delay": timedelta(minutes=0),
@@ -28,7 +27,6 @@ default_args = {
 }
 
 HOSPITAL = 'REAL HOSPITAL PORTGUES'
-
 
 def df_atendime():
     df = pd.read_sql(query_atendime, connect_rhp())
@@ -41,7 +39,7 @@ t0 = PythonOperator(
     task_id="insert_atendime_rhp",
     python_callable=df_atendime,
     op_kwargs={'connect_rhp': connect_rhp},
-    on_failure_callback=notify_email,
+    on_failure_callback=None,
     dag=dag)
 
 t0
