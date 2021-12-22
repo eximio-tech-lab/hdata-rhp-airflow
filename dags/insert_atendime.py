586d862513dc499703f5fa5c6c91d305b576717f
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2021-12-22 15:58:25 -0300

    Adicionado views que faltavam

--- dags/insert_atendime.py
+++ dags/insert_atendime.py
@@ -594,10 +594,10 @@ t28 = PythonOperator(
     python_callable=df_for_apl,
     dag=dag)
 
-# t29 = PythonOperator(
-#     task_id="insert_tip_esq_rhp",
-#     python_callable=df_tip_esq,
-#     dag=dag)
+t29 = PythonOperator(
+    task_id="insert_tip_esq_rhp",
+    python_callable=df_tip_esq,
+    dag=dag)
 
 t30 = PythonOperator(
     task_id="insert_tip_fre_rhp",
@@ -669,10 +669,10 @@ t43 = PythonOperator(
     python_callable=df_itmvto_estoque,
     dag=dag)
 
-# t44 = PythonOperator(
-#     task_id="insert_quantidade_diarias_rhp",
-#     python_callable=df_quantidade_diarias,
-#     dag=dag)
+t44 = PythonOperator(
+    task_id="insert_quantidade_diarias_rhp",
+    python_callable=df_quantidade_diarias,
+    dag=dag)
 
 t45 = PythonOperator(
     task_id="insert_remessa_fatura_rhp",
@@ -699,10 +699,10 @@ t49 = PythonOperator(
     python_callable=df_glosas,
     dag=dag)
 
-# t50 = PythonOperator(
-#     task_id="insert_custo_medio_mensal_rhp",
-#     python_callable=df_custo_medio_mensal,
-#     dag=dag)
+t50 = PythonOperator(
+    task_id="insert_custo_medio_mensal_rhp",
+    python_callable=df_custo_medio_mensal,
+    dag=dag)
 
 t51 = PythonOperator(
     task_id="insert_fa_custo_atendimento_rhp",
@@ -739,4 +739,4 @@ t57 = PythonOperator(
     python_callable=df_mot_dev,
     dag=dag)
 
-t0 >> t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7 >> t8 >> t9 >> t10 >> t11 >> t12 >> t13 >> t14 >> t15 >> t16 >> t17 >> t18 >> t19 >> t20 >> t21 >> t22 >> t23 >> t24 >> t25 >> t26 >> t27 >> t28 >> t30 >> t32 >> t33 >> t34 >> t35 >> t36 >> t37 >> t38 >> t39 >> t40 >> t41 >> t42 >> t43 >> t45 >> t46 >> t47 >> t48 >> t49 >> t51 >> t52 >> t53 >> t54 >> t55 >> t56 >> t57
+t0 >> t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7 >> t8 >> t9 >> t10 >> t11 >> t12 >> t13 >> t14 >> t15 >> t16 >> t17 >> t18 >> t19 >> t20 >> t21 >> t22 >> t23 >> t24 >> t25 >> t26 >> t27 >> t28 >> t29 >> t30 >> t32 >> t33 >> t34 >> t35 >> t36 >> t37 >> t38 >> t39 >> t40 >> t41 >> t42 >> t43 >> t44 >> t45 >> t46 >> t47 >> t48 >> t49 >> t50 >> t51 >> t52 >> t53 >> t54 >> t55 >> t56 >> t57
