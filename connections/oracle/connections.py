d648df335647fe7de6b3a627cf724beb65eeeb8f
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2021-12-23 20:24:43 -0300

    Tentando enconding no dsntns

--- connections/oracle/connections.py
+++ connections/oracle/connections.py
@@ -13,7 +13,7 @@ def connect_rhp():
 def connect_rhp_hdata():
     os.environ["NLS_LANG"] = ".UTF8"
     dsn_tns = cx_Oracle.makedsn('orclstage-1.cxp7emb18yqw.us-east-2.rds.amazonaws.com', 61521, service_name='orcl')
-    return cx_Oracle.connect('mv_rhp', 'zaDU0$qd', dsn_tns)
+    return cx_Oracle.connect('mv_rhp', 'zaDU0$qd', dsn_tns, encoding='UTF-8')
 
 def engine():
     engine = create_engine(connect_rhp(), max_identifier_length=128)
