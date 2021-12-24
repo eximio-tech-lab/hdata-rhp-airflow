834e158c808a171f7c68c56bed44f19484f374f3
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2021-12-24 11:08:39 -0300

    Mudando user/pwd

--- connections/oracle/connections.py
+++ connections/oracle/connections.py
@@ -11,9 +11,9 @@ def connect_rhp():
     return cx_Oracle.connect('hdata', 'hhhdatadata22191', dsn_tns)
 
 def connect_rhp_hdata():
-    # os.environ["NLS_LANG"] = ".UTF8"
-    # dsn_tns = cx_Oracle.makedsn('orclstage-1.cxp7emb18yqw.us-east-2.rds.amazonaws.com', 61521, service_name='orcl')
-    return cx_Oracle.connect('MV_RHP/MVRHP@orclstage-1.cxp7emb18yqw.us-east-2.rds.amazonaws.com:61521/orcl')
+    os.environ["NLS_LANG"] = ".UTF8"
+    dsn_tns = cx_Oracle.makedsn('orclstage-1.cxp7emb18yqw.us-east-2.rds.amazonaws.com', 61521, service_name='orcl')
+    return cx_Oracle.connect('MV_RHP', 'MVRHP', dsn_tns)
 
 def engine():
     engine = create_engine(connect_rhp(), max_identifier_length=128)
