1b34278f2402ca8ee5b0ba8c0bb7bb0305561566
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2021-12-27 18:14:33 -0300

    Teste insert

--- dags/insert_atendime.py
+++ dags/insert_atendime.py
@@ -54,7 +54,7 @@ def df_atendime():
 
     rows = [tuple(x) for x in df_diff.values]
 
-    cursor.execute("INSERT INTO MV_RHP.ATAENDIME VALUES (:1, :2, :3, :4)", rows)
+    cursor.execute("INSERT INTO MV_RHP.ATAENDIME VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20, :21, :22, :23, :24, :25, :26, :27, :28, :29, :30, :31, :32, :33, :34, :35, :36, :37, :38, :39, :40, :41)", rows)
 
     # df_diff.to_sql('MV_RHP.ATENDIME', con=engine_rhp(), if_exists='append', index=False, method='multi', chunksize=10000)
 
