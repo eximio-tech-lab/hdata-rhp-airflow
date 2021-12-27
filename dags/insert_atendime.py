832ec6f898131fa3e65d2a5e4039e7c6d4953cb6
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2021-12-27 17:41:48 -0300

    Teste insert

--- dags/insert_atendime.py
+++ dags/insert_atendime.py
@@ -48,7 +48,7 @@ def df_atendime():
     print('dados para incremento')
     print(df_diff.info())
 
-    df_diff.to_sql('MV_RHP.ATENDIME', connect_rhp_hdata(), if_exists='append', index=False)
+    df_diff.to_sql(name='MV_RHP.ATENDIME', con=connect_rhp_hdata(), if_exists='append', index=False, index=False, method='multi', chunksize=10000)
 
 def df_cid():
     print("Entrou no df_cid")
