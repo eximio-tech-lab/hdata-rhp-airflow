336c7528841b4da2f794a08259e5b95ebc19df0c
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2021-12-30 14:50:21 -0300

    Tempo Processos DH

--- dags/insert_atendime.py
+++ dags/insert_atendime.py
@@ -936,7 +936,7 @@ def df_sintoma_avaliacao():
 def df_tempo_processo():
     print("Entrou no df_tempo_processo")
 
-    df_dim = pd.read_sql(query_tempo_processo, connect_rhp())
+    df_dim = pd.read_sql(query_tempo_processo.format(data_ini="2021-01-01 00:00:00", data_fim="2021-12-31 23:59:59"), connect_rhp())
 
     print(df_dim)
 
