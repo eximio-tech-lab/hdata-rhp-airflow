ba098156b02bb36f6ebb59661e5f609c34ec5ca9
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-01-17 19:53:15 -0300

    Insert fato prescricao item

--- dags/insert_atendime.py
+++ dags/insert_atendime.py
@@ -59,10 +59,14 @@ def df_atendime():
     df_dim["NM_USUARIO_ALTA_MEDICA"] = df_dim["NM_USUARIO_ALTA_MEDICA"].fillna("0")
 
     print(df_dim.info())
+    print(df_dim['HR_ALTA'])
+    print(df_dim['HR_ALTA_MEDICA'])
 
     df_stage = pd.read_sql(query_atendime_hdata.format(data_ini='01/12/2021', data_fim='31/01/2022'), connect_rhp_hdata())
 
     print(df_stage.info())
+    print(df_stage['HR_ALTA'])
+    print(df_stage['HR_ALTA_MEDICA'])
 
     df_diff = df_dim.merge(df_stage,indicator = True, how='left').loc[lambda x : x['_merge'] !='both']
     df_diff = df_diff.drop(columns=['_merge'])
