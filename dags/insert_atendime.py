847f109ec56690adc7f363ff0376e8a4340469a5
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-01-17 17:55:04 -0300

    Teste update

--- dags/insert_atendime.py
+++ dags/insert_atendime.py
@@ -176,13 +176,13 @@ def df_atendime():
     df_dim["CD_SINTOMA_AVALIACAO"] = df_dim["CD_SINTOMA_AVALIACAO"].fillna(0)
     df_dim["NM_USUARIO_ALTA_MEDICA"] = df_dim["NM_USUARIO_ALTA_MEDICA"].fillna("0")
 
+    print(df_dim.info())
+
     df_stage = pd.read_sql(query_atendime_hdata.format(data_ini='01/10/2021', data_fim='31/10/2021'), connect_rhp_hdata())
 
     df_diff = df_dim.merge(df_stage,indicator = True, how='left').loc[lambda x : x['_merge'] !='both']
     df_diff = df_diff.drop(columns=['_merge'])
     df_diff = df_diff.reset_index(drop=True)
-
-    print(df_dim)
 
     print("dados para incremento")
     print(df_diff.info())
