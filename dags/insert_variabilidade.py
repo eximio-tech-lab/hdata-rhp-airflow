5b1d13ea9e947615659f67025594c864a18deb2c
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-01-21 10:50:12 -0300

    Captura variabilidade

--- dags/insert_variabilidade.py
+++ dags/insert_variabilidade.py
@@ -2726,4 +2726,4 @@ t57 = PythonOperator(
     python_callable=df_mot_dev,
     dag=dag)
 
-(t26, t27, t28, t29, t30, t31, t32, t33, t34, t35, t36, t37, t43, t44, t45, t49, t51, t52, t53, t54, t55, t56, t57) >> t38 >> t39 >> t40 >> t41 >> t42 >> t46 >> t47 >> t48 >> t50 >> t25
+(t26, t27, t28, t29, t30, t32, t33, t34, t35, t36, t37, t43, t44, t45, t49, t51, t52, t53, t54, t55, t56, t57) >> t38 >> t39 >> t40 >> t41 >> t42 >> t46 >> t47 >> t48 >> t50 >> t25
