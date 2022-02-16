aaa84a6ae6e7b3de43865573e0d9bda6707704a0
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-02-16 16:29:21 -0300

    Recapturando dados de outubro, novembro e dezembro de 2021

--- dags/insert_variabilidade_homol.py
+++ dags/insert_variabilidade_homol.py
@@ -67,7 +67,7 @@ def update_cells(df_eq, table_name, CD):
 
 def df_pre_med():
     print("Entrou no df_pre_med")
-    for dt in rrule.rrule(rrule.DAILY, dtstart=datetime.datetime(2021, 10, 1), until=datetime.datetime(2022, 1, 31)):
+    for dt in rrule.rrule(rrule.DAILY, dtstart=datetime.datetime(2021, 11, 7), until=datetime.datetime(2022, 1, 31)):
         data_1 = dt
         data_2 = dt
 
