7fe9dc9b0d24ea5473179b4d308f0f2b32dfcf05
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-11-03 11:35:14 -0300

    RHP - Captura dados dessa semana

--- dags/captura_dados_passado.py
+++ dags/captura_dados_passado.py
@@ -1510,7 +1510,7 @@ dt_ontem = datetime.datetime.today() - datetime.timedelta(days=1)
 dt_ini = dt_ontem - datetime.timedelta(days=5)
 
 # dag = DAG("insert_dados_rhp", default_args=default_args, schedule_interval=None)
-dag = DAG("captura_dados_rhp_antigos", default_args=default_args, schedule_interval="0 6 * * *")
+dag = DAG("captura_dados_rhp_antigos", default_args=default_args, schedule_interval=None)
 
 t0 = PythonOperator(
     task_id="captura_atendime_rhp",
