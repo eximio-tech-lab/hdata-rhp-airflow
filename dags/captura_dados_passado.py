a7890e9f1cf75e213f7ccf0e39b3cec98e48516f
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2022-12-02 14:30:01 -0300

    RHP - Solicitação LAB desde sempre

--- dags/captura_dados_passado.py
+++ dags/captura_dados_passado.py
@@ -60,7 +60,7 @@ def update_cells(df_eq, table_name, CD):
 
 def df_atendime():
     print("Entrou no df_atendime")
-dt_ini = datetime.datetime(2022, 3, 1)
+    dt_ini = datetime.datetime(2022, 3, 1)
     # for dt in rrule.rrule(rrule.DAILY, dtstart=datetime.datetime(2019, 1, 1), until=datetime.datetime(2021, 12, 31)):
     for dt in rrule.rrule(rrule.DAILY, dtstart=dt_ini, until=dt_ontem):
         data_1 = dt
