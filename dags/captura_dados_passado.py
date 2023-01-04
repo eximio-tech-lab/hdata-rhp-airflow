b67cc86696120de3d448509f38aab327e86c7493
Raphael Queiroz <raphael.queiroz@eximio.med.br>
2023-01-04 17:19:55 -0300

    RHP - Carga diária e retroativa

--- dags/captura_dados_passado.py
+++ dags/captura_dados_passado.py
@@ -453,7 +453,7 @@ def df_diagnostico_atendime(atendimentos):
 def df_documento_clinico():
     print("Entrou no df_documento_clinico")
     # for dt in rrule.rrule(rrule.MONTHLY, dtstart=datetime.datetime(2019, 1, 1), until=datetime.datetime(2021, 12,31)):
-    for dt in rrule.rrule(rrule.DAILY, dtstart=datetime.datetime(2019, 11, 13), until=datetime.datetime(2022, 11, 30)):
+    for dt in rrule.rrule(rrule.DAILY, dtstart=datetime.datetime(2020, 10, 15), until=datetime.datetime(2022, 11, 30)):
         data_1 = dt
         data_2 = dt
 
