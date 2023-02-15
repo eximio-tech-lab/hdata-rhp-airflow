05250b766b9dd223fdd24330b5710df6043122fb
Lucas R. Freire <lucas.freire@hdata.med.br>
2023-02-15 17:53:09 -0300

    Fix: removendo notificação

--- dags/testing_dag.py
+++ dags/testing_dag.py
@@ -36,32 +36,33 @@ def testing(**context):
 
 
 def update_all_cid():
-    for dt in rrule.rrule(rrule.DAILY, dtstart=datetime.datetime(2022,1,1), until=dt_ontem):
-        df_dim = pd.read_sql(query_update_all_cid.format(data=dt.strftime('%d/%m/%Y')), connect_rhp())
-        print("dados para incremento")
-        print(df_dim.info())
+    print('Ok!')
+    # for dt in rrule.rrule(rrule.DAILY, dtstart=datetime.datetime(2022,1,1), until=dt_ontem):
+    #     df_dim = pd.read_sql(query_update_all_cid.format(data=dt.strftime('%d/%m/%Y')), connect_rhp())
+    #     print("dados para incremento")
+    #     print(df_dim.info())
 
-        con = connect_rhp_hdata()
-        cursor = con.cursor()
+    #     con = connect_rhp_hdata()
+    #     cursor = con.cursor()
 
-        sql="INSERT INTO MV_RHP.UPDATE_CID_TEMP (CD_ATENDIMENTO, CD_MULTI_EMPRESA, CD_CID) VALUES (:1, :2, :3)"
+    #     sql="INSERT INTO MV_RHP.UPDATE_CID_TEMP (CD_ATENDIMENTO, CD_MULTI_EMPRESA, CD_CID) VALUES (:1, :2, :3)"
 
-        df_list = df_dim.values.tolist()
-        n = 0
-        cols = []
-        for i in df_dim.iterrows():
-            cols.append(df_list[n])
-            n += 1
+    #     df_list = df_dim.values.tolist()
+    #     n = 0
+    #     cols = []
+    #     for i in df_dim.iterrows():
+    #         cols.append(df_list[n])
+    #         n += 1
 
-        cursor.executemany(sql, cols)
-        con.commit()
-        cursor.close
-        con.close
-    error_message("CIDs ATUALIZADOS RHP",
-            ["lucas.freire@hdata.med.br","raphael.queiroz@hdata.med.br"],
-            ["mensagem vinda do RHP",
-            "sucesso"],
-            type='Stage')
+    #     cursor.executemany(sql, cols)
+    #     con.commit()
+    #     cursor.close
+    #     con.close
+    #error_message("CIDs ATUALIZADOS RHP",
+    #        ["lucas.freire@hdata.med.br","raphael.queiroz@hdata.med.br"],
+    #        ["mensagem vinda do RHP",
+    #        "sucesso"],
+    #        type='Stage')
     
     print("Dados inseridos")
 
